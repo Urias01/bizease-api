00042be635c85c919478063cf29d4bf6bfdc0e22
Bhendon Soares <bhendon.soares12@gmail.com>
2024-06-30 22:58:09 -0300

    Create a Java & Spring Boot project

---
+++ src/main/java/com/bizease/api/app/BizeaseApplication.java
@@ -0,0 +1,13 @@
+package com.bizease.api.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizease/api/app/BizeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizease.api.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
