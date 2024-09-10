bb5fed63029e7fb837608cbdaea9c553d5fd1671
Bhendon Soares <bhendon.soares12@gmail.com>
2024-09-10 14:20:46 -0300

    feat: add get all purchase orders

--- src/main/java/com/bizease/api/app/model/purchase_orders/controller/PurchaseOrdersController.java
+++ src/main/java/com/bizease/api/app/model/purchase_orders/controller/PurchaseOrdersController.java
@@ -1,13 +1,16 @@
 package com.bizease.api.app.model.purchase_orders.controller;
 
 import com.bizease.api.app.model.purchase_orders.dto.PurchaseOrdersRequestDTO;
+import com.bizease.api.app.model.purchase_orders.entities.PurchaseOrders;
 import com.bizease.api.app.model.purchase_orders.useCases.CreatePurchaseOrdersUseCase;
+import com.bizease.api.app.model.purchase_orders.useCases.GetAllPurchaseOrdersUseCase;
 import com.bizease.api.app.model.purchase_orders.useCases.GetPurchaseOrderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,6 +22,9 @@ public class PurchaseOrdersController {
 
     @Autowired
     private GetPurchaseOrderUseCase getPurchaseOrderUseCase;
+
+    @Autowired
+    GetAllPurchaseOrdersUseCase getAllPurchaseOrdersUseCase;
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_OWNER')")
@@ -40,5 +46,12 @@ public class PurchaseOrdersController {
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
         }
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_OWNER')")
+    public ResponseEntity<List<PurchaseOrders>> getAllPurchaseOrders() {
+        var result = this.getAllPurchaseOrdersUseCase.execute();
+        return ResponseEntity.ok(result);
     }
 }
