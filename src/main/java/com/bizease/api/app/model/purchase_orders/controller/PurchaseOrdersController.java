e2b3599df85b2a0b01c393cecd52c5dc40806406
Bhendon Soares <bhendon.soares12@gmail.com>
2024-09-10 14:12:44 -0300

    feat: add get purchase order by uuid funcionality

--- src/main/java/com/bizease/api/app/model/purchase_orders/controller/PurchaseOrdersController.java
+++ src/main/java/com/bizease/api/app/model/purchase_orders/controller/PurchaseOrdersController.java
@@ -2,13 +2,13 @@ package com.bizease.api.app.model.purchase_orders.controller;
 
 import com.bizease.api.app.model.purchase_orders.dto.PurchaseOrdersRequestDTO;
 import com.bizease.api.app.model.purchase_orders.useCases.CreatePurchaseOrdersUseCase;
+import com.bizease.api.app.model.purchase_orders.useCases.GetPurchaseOrderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/purchase_orders")
@@ -17,11 +17,25 @@ public class PurchaseOrdersController {
     @Autowired
     private CreatePurchaseOrdersUseCase createPurchaseOrdersUseCase;
 
+    @Autowired
+    private GetPurchaseOrderUseCase getPurchaseOrderUseCase;
+
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_OWNER')")
     public ResponseEntity<Object> createPurchaseOrder(@RequestBody PurchaseOrdersRequestDTO purchaseOrdersRequestDTO) {
         try {
             var result = this.createPurchaseOrdersUseCase.execute(purchaseOrdersRequestDTO);
+            return ResponseEntity.ok(result);
+        } catch (Exception exception) {
+            return ResponseEntity.badRequest().body(exception.getMessage());
+        }
+    }
+
+    @GetMapping("/{uuid}")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_OWNER')")
+    public ResponseEntity<Object> getPurchaseOrder(@PathVariable UUID uuid) {
+        try {
+            var result = this.getPurchaseOrderUseCase.execute(uuid);
             return ResponseEntity.status(201).body(result);
         } catch (Exception exception) {
             return ResponseEntity.badRequest().body(exception.getMessage());
---
+++ src/main/java/com/bizease/api/app/model/purchase_orders/useCases/GetPurchaseOrderUseCase.java
@@ -0,0 +1,22 @@
+package com.bizease.api.app.model.purchase_orders.useCases;
+
+import com.bizease.api.app.exceptions.NotFoundException;
+import com.bizease.api.app.model.purchase_orders.entities.PurchaseOrders;
+import com.bizease.api.app.model.purchase_orders.repository.PurchaseOrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class GetPurchaseOrderUseCase {
+
+    @Autowired
+    private PurchaseOrdersRepository purchaseOrdersRepository;
+
+    public PurchaseOrders execute(UUID uuid) {
+        return this.purchaseOrdersRepository
+                .findByUuid(uuid).orElseThrow(() -> new NotFoundException("Pedido de compra"));
+    }
+}
