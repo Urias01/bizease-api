1133065100a63cc4b223535314772843d5c8f8f5
Urias01 <uriasluis64@gmail.com>
2025-09-17 21:49:27 -0300

    refactor: update ApiResponse

--- src/main/java/com/bizease/api/app/responses/ApiResponse.java
+++ src/main/java/com/bizease/api/app/responses/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.bizease.api.app.responses;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +11,31 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
-  
+
   private boolean success;
   private int total;
   private T data;
-  
+  private int code;
+  private String message;
+  private String stackTrace;
+
+  public static <T> ApiResponse<T> success(T data, int code) {
+    ApiResponse<T> response = new ApiResponse<T>();
+    response.success = true;
+    response.data = data;
+    response.code = code;
+    return response;
+  }
+
+  public static <T> ApiResponse<T> error(String message, String stackTrace, int code) {
+    ApiResponse<T> response = new ApiResponse<T>();
+    response.success = false;
+    response.code = code;
+    response.message = message;
+    response.stackTrace = stackTrace;
+    return response;
+  }
+
 }
