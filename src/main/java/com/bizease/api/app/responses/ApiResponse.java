b144bbe225fac2cf3c9254195ab361bfeb384f43
Urias01 <uriasluis64@gmail.com>
2025-09-18 15:19:08 -0300

    chore: update api response

--- src/main/java/com/bizease/api/app/responses/ApiResponse.java
+++ src/main/java/com/bizease/api/app/responses/ApiResponse.java
@@ -15,27 +15,22 @@ import lombok.Setter;
 public class ApiResponse<T> {
 
   private boolean success;
-  private int total;
+  private Integer total;
   private T data;
   private int code;
   private String message;
   private String stackTrace;
 
   public static <T> ApiResponse<T> success(T data, int code) {
-    ApiResponse<T> response = new ApiResponse<T>();
-    response.success = true;
-    response.data = data;
-    response.code = code;
-    return response;
+    return new ApiResponse<>(true, null, data, code, null, null);
+  }
+
+  public static <T> ApiResponse<T> success(T data, int code, int total) {
+    return new ApiResponse<>(true, total, data, code, null, null);
   }
 
   public static <T> ApiResponse<T> error(String message, String stackTrace, int code) {
-    ApiResponse<T> response = new ApiResponse<T>();
-    response.success = false;
-    response.code = code;
-    response.message = message;
-    response.stackTrace = stackTrace;
-    return response;
+    return new ApiResponse<>(false, null, null, code, message, stackTrace);
   }
 
 }
