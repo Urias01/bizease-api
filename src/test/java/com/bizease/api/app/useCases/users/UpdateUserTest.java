ed49dff1bdd1e17f1ebe5bb1a164916239154a78
Urias01 <uriasluis64@gmail.com>
2025-11-13 13:38:05 -0300

    test: added update user test

--- src/test/java/com/bizease/api/app/useCases/users/UpdateUserTest.java
+++ src/test/java/com/bizease/api/app/useCases/users/UpdateUserTest.java
@@ -1,5 +1,67 @@
 package com.bizease.api.app.useCases.users;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.bizease.api.app.exceptions.NotFoundException;
+import com.bizease.api.app.models.User;
+import com.bizease.api.app.models.request.UpdateUserRequest;
+import com.bizease.api.app.repositories.UserRepository;
+import com.bizease.api.app.security.jwt.IJwtAuthContext;
+
+@ExtendWith(MockitoExtension.class)
 public class UpdateUserTest {
-  
+
+  @Mock
+  private UserRepository userRepository;
+  @Mock
+  private IJwtAuthContext jwtAuthContext;
+
+  @InjectMocks
+  private UpdateUser updateUser;
+
+  @Test
+  @DisplayName("Should be able to update a user")
+  public void shouldBeAbleToUpdateUser() {
+
+    User user = new User();
+    user.setId("user-id");
+    user.setName("John Doe");
+
+    when(jwtAuthContext.getTenantId()).thenReturn("tenant-id");
+    when(userRepository.findByIdAndTenantId("user-id", "tenant-id"))
+        .thenReturn(Optional.of(user));
+
+    UpdateUserRequest request = new UpdateUserRequest("Jane Doe", "email@test.com");
+    updateUser.execute("user-id", request);
+
+    User updatedUser = userRepository.findByIdAndTenantId("user-id", "tenant-id").orElse(null);
+
+    assertNotNull(updatedUser);
+    assertEquals("Jane Doe", updatedUser.getName());
+    assertEquals("email@test.com", updatedUser.getEmail());
+  }
+
+  @Test
+  @DisplayName("Should throw exception when user not found")
+  public void shouldThrowExceptionWhenUserNotFound() {
+    UpdateUserRequest request = new UpdateUserRequest("Jane Doe", "email@test.com");
+
+    when(jwtAuthContext.getTenantId()).thenReturn("tenant-123");
+    when(userRepository.findByIdAndTenantId("user-id", "tenant-123"))
+        .thenReturn(Optional.empty());
+
+    assertThrows(NotFoundException.class, () -> updateUser.execute("user-id", request));
+  }
 }
