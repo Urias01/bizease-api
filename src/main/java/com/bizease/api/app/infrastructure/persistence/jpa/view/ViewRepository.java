f0765c9fca01ec80f456e94a5216503bc6db40b6
Urias01 <uriasluis64@gmail.com>
2025-12-08 19:17:47 -0300

    remove unnecessary method

--- src/main/java/com/bizease/api/app/infrastructure/persistence/jpa/view/ViewRepository.java
+++ src/main/java/com/bizease/api/app/infrastructure/persistence/jpa/view/ViewRepository.java
@@ -1,7 +1,5 @@
 package com.bizease.api.app.infrastructure.persistence.jpa.view;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -12,8 +10,6 @@ import com.bizease.api.app.models.filters.ViewFilter;
 
 public interface ViewRepository extends JpaRepository<View, String>, JpaSpecificationExecutor<View>  {
 
-  Optional<View> findById(String id);
-  
   @Query("SELECT v FROM View v WHERE (:#{#filter.id} IS NULL OR v.id = :#{#filter.id}) "
       + "AND (:#{#filter.name} IS NULL OR LOWER(v.name) LIKE LOWER(CONCAT('%', :#{#filter.name}, '%'))) "
       + "AND (:#{#filter.status} IS NULL OR v.status = :#{#filter.status})")
